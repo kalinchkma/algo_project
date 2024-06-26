cbe31df424425530fcb28c06812df173b3720017
kalinchkma <chakma.kalin10211@gmail.com>
2024-06-27 00:50:15 +0600

    bubble short added

--- algo/src/dsa/bubble_short.rs
+++ algo/src/dsa/bubble_short.rs
@@ -1,15 +1,24 @@
 
 fn bubble_sort(mut v: Vec<i32>) -> Vec<i32> {
 
-    for (i, n) in v.iter().enumerate() {
-        if (i < v.len()) {
-          println!("{}, {}", n, v[i+1])
+    for i in 0..v.len() {
+        let mut swap = false;
+        for j in 0..v.len()-i-1 {
+            if v[j] > v[j+1] {
+                let temp = v[j];
+                v[j] = v[j+1];
+                v[j+1] = temp;
+                swap = true
+            }
+        }
+        if !swap {
+            break;
         }
     }
-
-    vec![]
+    println!("{:?}", v);
+    v
 }
 
 pub fn run() {
-    bubble_sort(vec![3, 5,13, 76]);
+    bubble_sort(vec![35, 5,1, 77, 9, 76]);
 }
