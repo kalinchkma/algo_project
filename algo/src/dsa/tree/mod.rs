22d5d5e2d14ee270fb2afa1332133b346053eb3c
kalinchkma <chakma.kalin10211@gmail.com>
2024-06-30 01:53:38 +0600

    working on binary trees

--- algo/src/dsa/tree/mod.rs
+++ algo/src/dsa/tree/mod.rs
@@ -21,3 +21,33 @@ impl<T> Node<T> {
 struct BinaryTree<T> {
     root: Option<Box<Node<T>>>
 }
+
+impl<T: Ord> BinaryTree<T> {
+    fn new() -> Self {
+        BinaryTree {
+            root: None
+        }
+    }
+
+    fn insert(&mut self, value: T) {
+        let new_node = Box::new(Node::new(value));
+        match self.root {
+            None => self.root = Some(new_node),
+            Some(ref mut node) => Self::insert_node( node, new_node),
+        }
+    }
+
+    fn insert_node(current: &mut Box<Node<T>>, new_node: Box<Node<T>>) {
+        if new_node.value < current.value {
+            match current.left {
+                None => current.left = Some(new_node),
+                Some(ref mut left_child) => Self::insert_node(left_child, new_node)
+            }
+        } else {
+            match current.right {
+                None => current.right = Some(new_node),
+                Some(ref mut right_child) => Self::insert_node(right_child, new_node)
+            }
+        }
+    }
+}
