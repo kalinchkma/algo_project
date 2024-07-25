5a2a9784a2748fe9d6606d79ca0ff0aeb7ba749b
kalinchkma <chakma.kalin10211@gmail.com>
2024-07-26 02:12:17 +0600

    working with permutation

--- problem_solving/generate_permutation.rs
+++ problem_solving/generate_permutation.rs
@@ -2,20 +2,20 @@
 
 
 fn main() {
-    let numbers: Vec<i32> = vec![1, 2, 3];
+    let numbers: Vec<i32> = vec![3, 6, 9];
     let n = numbers.len() as usize;
     let mut permutation: Vec<i32> = Vec::new();
     let mut chosen: Vec<bool> = vec![false; n];
     
-    search(&mut permutation, n, &mut chosen);
+    search(&mut permutation, n, &mut chosen, &numbers);
 }
 
-fn search(permutation: &mut Vec<i32>, n: usize, chosen: &mut Vec<bool>) {
+fn search(permutation: &mut Vec<i32>, n: usize, chosen: &mut Vec<bool>, sets: &Vec<i32>) {
         if permutation.len() == n {
             // process permutation
             println!("Permutation");
             for p in permutation {
-                print!("{},", p);
+                print!("{},", sets[*p as usize]);
             }
             println!("");
         } else {
@@ -23,7 +23,7 @@ fn search(permutation: &mut Vec<i32>, n: usize, chosen: &mut Vec<bool>) {
                 if chosen[i as usize] { continue};
                 chosen[i as usize] = true;
                 permutation.push(i as i32);
-                search(permutation, n, chosen);
+                search(permutation, n, chosen, sets);
                 chosen[i as usize] = false;
                 permutation.pop();
             }
